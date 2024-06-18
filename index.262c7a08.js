918ff23b76ce9b9aee2488955c3b07d6afb6e11f
Oleksandr Lytovchenko <jaster93him@gmail.com>
2024-06-18 21:05:25 +0300

    make build

---
+++ index.262c7a08.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target;n.classList.contains("gallery__thumb")?e.src=n.parentElement.href:e.src=n.href});
+//# sourceMappingURL=index.262c7a08.js.map
